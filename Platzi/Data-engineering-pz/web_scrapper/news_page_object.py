df00af4c7d422cc72710a4738c1ce9ed219668f9
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-04-15 12:57:51 -0500

    news_page_object corrected

--- Platzi/Data-engineering-pz/web_scrapper/news_page_object.py
+++ Platzi/Data-engineering-pz/web_scrapper/news_page_object.py
@@ -9,14 +9,15 @@ class HomePage:
         self._config = config()["news_sites"][news_site_uid]
         self._queries = self._config["queries"]
         self._html = None
+        self._visit(url)
 
     @property
     def article_links(self):
-        links_list = []
+        link_list = []
         for link in self._select(self._queries["homepage_article_links"]):
             if link and link.has_attr("href"):
                 link_list.append(link)
-        return set(link["href"] for link in link_lis)
+        return set(link["href"] for link in link_list)
 
     def _select(self,query_string):
         return self._html.select(query_string)
