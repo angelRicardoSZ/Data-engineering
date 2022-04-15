f95e4ca70984b98b533543316e8eb7f771257c7a
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-04-15 15:23:07 -0500

    Se cambio el sito a scrapear debido a que se requerian permisos, se cambio por el sitio de forbes, como lo hizo un estudiante de platzi(Francisco Garcia) con lo cual se obtuvieron los links de las noticias sin problemas: el estado del sistema es en funcionamiento

--- Platzi/Data-engineering-pz/web_scrapper/common.py
+++ Platzi/Data-engineering-pz/web_scrapper/common.py
@@ -7,7 +7,7 @@ __config = None
 def config():
     global __config
     if not __config:
-        with open("config.yaml",mode="r") as f:
+        with open("config2.yaml",mode="r") as f:
             __config = yaml.safe_load(f)
 
     return __config
