f70a62876d7b94a0d1b7a1f47d8d7b80bb7d579b
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-04-15 08:11:29 -0500

    creation of web scraper

---
+++ Platzi/Data-engineering-pz/web_scrapper/main.py
@@ -0,0 +1,21 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+from common import config
+
+logger = logging.getLogger(__name__)
+
+
+def _news_scraper(news_site_uid):
+    host = config()["new_sites"][news_site_uid]["url"]
+    logging.info('beggining scrapper for {}'.format(host))
+    logging.info('FIndind links')
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    new_sites_choices = list(config()["new_sites"].keys())
+    parser.add_argument("new_site",help="The new site that you want to scrape",type=str,choices=new_sites_choices)
+    args = parser.parse_args()
+    _news_scraper(args.new_site)
+
