8c26f89a6e420a5b3ab44788df5a166d2857c735
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-04-15 23:07:24 -0500

    Se termino el código para poder leer articulos y obtener el cuerpo y el titulo, pero aun se obteien algunos errores

--- Platzi/Data-engineering-pz/web_scrapper/main.py
+++ Platzi/Data-engineering-pz/web_scrapper/main.py
@@ -1,10 +1,16 @@
 import argparse
 import logging
 logging.basicConfig(level=logging.INFO)
+import re
+
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 import news_page_object as news
 from common import config
 
 logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^ḧttps?://.+/.+$')
+is_root_path = re.compile(r'^/.+$')
 
 
 def _news_scraper(news_site_uid):
@@ -12,9 +18,39 @@ def _news_scraper(news_site_uid):
     logging.info('beggining scrapper for {}'.format(host))
     logging.info('FIndind links')
     homepage = news.HomePage(news_site_uid, host)
-
+    articles = []
     for link in homepage.article_links:
         print(link)
+        artricle = _fetch_article(news_site_uid,host,link)
+        if article:
+            logger.info('Article fetched!!')
+            articles.append(article)
+            print(article.title)
+    print(len(article))
+
+
+
+
+def _fetch_artice(news_site_uid, host, link):
+    logger.info('Start fetching article at {}'.format(link))
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host,link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article',exc_info=False)
+    
+    if article and not article.body:
+        logger.warning('Invalid article. There is no body')
+        return None
+    return article
+
+def _build_link(host,link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return '{}{}'.format(host,link)
+    else:
+        return '{host}/{uri}'.format(host=host,uri=link)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
