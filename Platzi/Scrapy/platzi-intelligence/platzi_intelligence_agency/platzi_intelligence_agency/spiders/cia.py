c0205f2f4d9a57d3e6ca6dd475034b8cea913515
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-05-07 06:07:17 -0500

    Scrapy. final project finished: Extracting data from website, transforming to jsfon format and loading into another website

--- Platzi/Scrapy/platzi-intelligence/platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
+++ Platzi/Scrapy/platzi-intelligence/platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
@@ -19,14 +19,18 @@ class SpiderCIA(scrapy.Spider):
     }
     
     def parse(self, response):
-        links_declassified = response.xpath('//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]').getall()
+        links_declassified = response.xpath('//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href').getall()
         for link in links_declassified:
+            #print("----")
+            #print(link)
+            #print("----")
             yield response.follow(link, callback= self.parse_link, cb_kwargs={'url':response.urljoin(link)})
             
     def parse_link(self,response, **kwargs):
         link = kwargs['url']
         title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
-        paragraph = response.xpath('//div[@class="field-item even"]//p[not(@class)]/text()').get()
+        paragraph = response.xpath('//div[@class="field-item even"]//p[not(@class) and not(child::strong)]/text()').getall()
+        paragraph = ' '.join(paragraph)
         yield {
             'url':link,
             'title':title,
